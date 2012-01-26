ca485bf8d205d0dad77a03a0eb5e40f7f0e0b6b3
Tristan Lins <tristan.lins@infinitysoft.de>
2012-01-26 16:03:29 +0100

    Initial import

---
+++ system/modules/HidePageTitle/languages/en/tl_page.php
@@ -0,0 +1,39 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Hide page title
+ * Copyright (C) 2010,2011 Tristan Lins
+ *
+ * Extension for:
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  InfinitySoft 2010,2011
+ * @author     Tristan Lins <tristan.lins@infinitysoft.de>
+ * @package    HidePageTitle
+ * @license    LGPL
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['doNotShowPageTitle'] = array('Do not show page title', 'Hide the page title and only show the website title in the title bar.');
